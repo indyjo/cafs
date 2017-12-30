fedc6d0a3d767836feb6661a65f1bf6d9b6d050d
Jonas Eschenburg <jonas@bitwrk.net>
2017-12-30 21:41:47 +0100

    Fix test in cafs/remotesync/shuffle

--- remotesync/shuffle/shuffle_test.go
+++ remotesync/shuffle/shuffle_test.go
@@ -32,27 +32,66 @@ func shuffleString(t *testing.T, in string, s StreamShuffler) (out string) {
 	return
 }
 
+// Test that doing multiple simultaneous transmissions via a buffered connection to a simultated
+// caching receiver actually reduces the amount of data per transmission to the expected degree.
 func TestTransmission(t *testing.T) {
-	r := rand.New(rand.NewSource(0))
-	data := make([]int, 2048)
-	for idx := range data {
-		data[idx] = r.Int()
-	}
-	received := make(map[int]bool)
+	const NTRANSMISSIONS = 3
+	const BUFFER_SIZE = 1000
+	const PERMUTATION_SIZE = 8000
 
-	type transmisson struct {
-		sent     int
-		shuffler StreamShuffler
-		buf      chan int
-	}
-	transmissions := make([]transmission, 4)
-	for idx := range transmissions {
-		transmissions[idx].shuffler = NewStreamShuffler(Random(128, r), func(v interface{}) error {
-			if !received[v.(int)] {
-				received[v.(int)] = true
-				transmissions[idx].sent++
+	// Simulate a cache of already received data. The value expresses at which time step the data is received.
+	// That's how we can simulate buffering behavior.
+	received := make(map[int]int)
+	// Counter of data that was actually sent
+	sent := 0
+
+	// Time step.
+	time := 0
+
+	// Create a number of shufflers that simulate parallel transmissions.
+	transmissions := make([]StreamShuffler, NTRANSMISSIONS)
+
+	r := rand.New(rand.NewSource(0))
+	for i := range transmissions {
+		transmissions[i] = NewStreamShuffler(Random(PERMUTATION_SIZE, r), func(v interface{}) error {
+			if v.(int) >= 0 {
+				// Test if data has arrived at cache yet. If not, put it in and trigger retransmission
+				if t_received, ok := received[v.(int)]; !ok || time < t_received {
+					if !ok {
+						received[v.(int)] = time + BUFFER_SIZE
+					}
+					sent++
+				}
 			}
+			return nil
 		})
 	}
 
+	for i := 0; i < PERMUTATION_SIZE; i++ {
+		for _, transmission := range transmissions {
+			if err := transmission.Put(i); err != nil {
+				t.Fatalf("Unexpected error: %v", err)
+			}
+		}
+		time++
+	}
+
+	// Purposefully don't call End() on transmissions because that would not simulate parallel behavior.
+	// Instead, flood the shufflers with a dummy value in parallel. By putting PERMUTATION_SIZE
+	// dummy values per transmission, we can guarantee the buffer was flushed.
+	for i := 0; i < PERMUTATION_SIZE; i++ {
+		for _, transmission := range transmissions {
+			if err := transmission.Put(-1); err != nil {
+				t.Fatalf("Unexpected error: %v", err)
+			}
+		}
+		time++
+	}
+
+	transmission_avg := float64(sent) / float64(PERMUTATION_SIZE)
+	t.Logf("Transmissions/data: % 5.2f", transmission_avg)
+
+	// This model is probably wrong because it assumes stochastic independence of data chunk transmissions
+	t.Logf("Expected:           % 5.2f", 1+float64(NTRANSMISSIONS-1)*float64(BUFFER_SIZE)/float64(PERMUTATION_SIZE))
+	// TODO: Add actual test here
 }
